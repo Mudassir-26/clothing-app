d06b5d0a2af29d67e9fcf9249d1e3a1e24797da9
Anindita Patnayak <aninditapattnayak0@gmail.com>
2025-07-17 13:34:37 +0530

    Create CreateProduct.jsx

---
+++ client/src/pages/CreateProduct.jsx
@@ -0,0 +1,14 @@
+//client/src/pages/CreateProduct.jsx
+import React from 'react';
+import ProductForm from '../components/ProductForm';
+
+const CreateProduct = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-center mt-4">Create Product</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default CreateProduct;
